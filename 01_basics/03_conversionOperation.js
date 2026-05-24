68bd97347024f6369e280edd1053cafb34949560
poonamkevat <POONAMKEVAT16@GMAIL.COM>
2026-05-24 19:22:15 +0530

    operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,29 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//************************Operations/************************/ /
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+//console.log(2**3);
+
+let str1 = "poonam "
+let str2 = "kevat"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true)
+// console.log(+"");
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operation.html#sec-type-conversion
